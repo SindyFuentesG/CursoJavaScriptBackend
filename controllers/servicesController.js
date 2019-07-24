652ceb4b8cef9e0b1bceb685a3100fef2ce1fb4d
Sindy Tatiana Fuentes Gutierrez <sindy.fuentes@Quipux.local>
2019-07-24 12:30:51 -0500

    Implementación para obtener información de los servicios por id

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -2,7 +2,7 @@ var serviceModel = require('./../models/serviceModel');
 
 exports.services_all_get = function (req, res){
     serviceModel.findAll({
-        attributes:['id', 'name', 'image', 'happy']
+        attributes:['id', 'name', 'image', 'happy', 'cost', 'description']
     })
     .then(data => res.json(data))
     .catch (err => res.status(500).send(err))
@@ -10,10 +10,16 @@ exports.services_all_get = function (req, res){
 }
 
 exports.services_detail_get = function (req, res){
-    serviceModel.findAll({
+    let id = Number(req.params.id);
+    serviceModel.findByPk(id,
+        {attributes: ['id', 'name', 'image', 'happy', 'cost', 'description']
+    })
+    .then(data => res.json(data))
+    .catch(err => res.status(500).send('error query'))
+   /* serviceModel.findAll({
         attributes:['id', 'name', 'image', 'happy'], where: {id: req.params.id}
     })
     .then(data => res.json(data))
-    .catch (err => res.status(500).send(err))
+    .catch (err => res.status(500).send(err))*/
 }
 
